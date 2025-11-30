55bc534117e9e2385c2d190d34262cd1318cf704
conormvickers <conormvickers@gmail.com>
2025-11-30 09:58:08 -0500

    working!

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,27 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const baudRate = 115200;
 function App() {
   const [serialPort, setSerialPort] = useState<SerialPort | null>(null);
+
+  async function connectSerial() {
+    await serialPort.open({ baudRate: baudRate });
+  }
+
+  async function sendSerial(data: string) {
+    if (serialPort) {
+      const writer = serialPort.writable.getWriter();
+      const dataToSend = new TextEncoder().encode(data);
+      await writer.write(dataToSend);
+      writer.releaseLock();
+    }
+  }
+  useEffect(() => {
+    if (serialPort) {
+      connectSerial();
+    }
+  }, [serialPort]);
 
   async function selectSerialPort() {
     if (!navigator.serial) {
@@ -25,7 +44,17 @@ function App() {
 
   return (
     <>
+      {serialPort && serialPort.connected ? "Connected" : "Disconnected"}
+
       <button onClick={selectSerialPort}>Select Serial Port</button>
+      <button onClick={() => sendSerial("x")}>Move X</button>
+      <button onClick={() => sendSerial("y")}>Move Y</button>
+
+      <button onClick={() => sendSerial("h")}>Home X</button>
+      <button onClick={() => sendSerial("j")}>Home Y</button>
+      <div>
+        <button onClick={() => sendSerial("n")}>Next Bin</button>
+      </div>
     </>
   );
 }
