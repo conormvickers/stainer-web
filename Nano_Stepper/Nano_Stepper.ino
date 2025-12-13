9c5a0cadcd546e2a48b4f0ea7ccd0ba75d4f61c0
Conor <conormvickers@gmail.com>
2025-12-13 03:16:26 +0000

    700 y

--- Nano_Stepper/Nano_Stepper.ino
+++ Nano_Stepper/Nano_Stepper.ino
@@ -173,7 +173,7 @@ void jumpy (){
   
   WebSerial.send("event-from-arduino", "starting");
 
-  move(600, HIGH, 'y', 0);
+  move(700, HIGH, 'y', 0);
 
   WebSerial.send("event-from-arduino", "done " + String(totalcounterx) + " " + String(totalcountery));
 
